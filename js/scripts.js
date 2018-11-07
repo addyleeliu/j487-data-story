b94e4ffd7f8d3beedda6f3262a7861449f1f9d28
Addy <addyleeliu@gmail.com>
2018-11-07 09:51:19 -0500

    added table of Emissions by Producer

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,31 @@ $(function() {
 
   console.log('jQ loaded all systems go');
 
+  $('#ajax-table').DataTable({
+    "ajax": "/js/ghg_producers.txt",
+    "columns": [
+      {"data": "producer"},
+      {"data": "ghg_percent"}
+    ],
+    "columnDefs": [{
+      "targets": [0, 1],
+      "createdCell": function(td, cellData, rowData, row, col) {
+        if(cellData > 2) {
+          $(td).css('color', 'red');
+        }
+        $(td).on({
+          mouseenter: function() {
+            var txt = $(this).text();
+            console.log(txt);
+            $('#' + txt).toggleClass('hidden');
+          },
+          mouseleave: function() {
+            var txt = $(this).text(); // have to redeclare variable bc it's local
+            $('#' + txt).toggleClass('hidden');
+          }
+        });
+      } // close createdCell
+    }] // close columnDefs
+  }); // close DataTables
+
 });
