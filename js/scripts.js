bd63439c8570d0475c3169cbf8a268003880ec4d
Addy <addyleeliu@gmail.com>
2018-11-07 20:05:39 -0500

    added chart of global temperature anomalies

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,77 @@ $(function() {
     }] // close columnDefs
   }); // close DataTables
 
+
+
+
+
+
+
+
+
+// BUILD A TABLE WITH AJAX LOADED DATA
+var url = 'js/temp_anomalies.json';
+var data = [];
+var xCat = []; // categories on the x axis (bottom of chart)\
+var tempAnomalies = []; // y axis values
+
+$.ajax({
+  type: 'GET',
+  dataType: 'json',
+  data: data,
+  url: url,
+  async: true,
+  success: function(data) {
+    console.log(data);
+
+    for (i=0; i < data.length; i++) {
+      // LOOP THROUGH THE DATA AND BUILD AN ARRAY
+      xCat.push(data[i].year);
+      tempAnomalies.push(data[i].temp_anomaly);
+    }
+    buildChart();
+  }
+}); // close ajax call
+
+function buildChart() {
+  var myChart = Highcharts.chart('ajax-chart', {
+      chart: {
+          type: 'column',
+          marginTop: 15,
+          spacingBottom: 100
+      },
+      title: {
+          text: null
+      },
+      subtitle: {
+          text: 'Source: <a href="https://climate.nasa.gov/vital-signs/global-temperature/">NASA Goddard Institute for Space Studies (GISS)</a>',
+          verticalAlign: 'bottom',
+          y: 50
+      },
+      xAxis: {
+          categories: xCat
+      },
+      yAxis: {
+          title: {
+              text: 'Temperature Anomaly (ºC) Relative to 1951-1980 Average Temperatures'
+          }
+      },
+      plotOptions: {
+        column: {
+            zones: [{
+                value: 0,
+                  color: 'blue' // Negative values are blue
+              },{
+                color: 'red' // Non-negative values are red (0 and up)
+              }]
+          }
+      },
+      series: [{
+          name: 'Temperature Anomaly (ºC)',
+          showInLegend: false,
+          data: tempAnomalies
+      }]
+  }); // close myChart
+} // close buildChart
+
 });
